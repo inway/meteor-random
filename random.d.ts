952e713244bb2480de4e1a16442a932fa0d1524a
Sebastian Podjasek <sebastian.podjasek@intelliway.pl>
2021-02-17 15:43:37 +0100

    Add missing methods

--- random.d.ts
+++ random.d.ts
@@ -1,7 +1,10 @@
 export default Random;
 
-export declare class Random {
-    static id(charsCount: number): string;
-    static secret(charsCount: number): string;
-    static choice(): string;
+declare class Random {
+    static choice(array: any[]): string;
+    static choice(str: string): string;
+    static fraction(): number;
+    static id(charsCount?: number): string;
+    static hexString(numberOfDigits: number): string;
+    static secret(charsCount?: number): string;
 }
