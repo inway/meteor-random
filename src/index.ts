c4f0301198abdaabae5c5b79e3e4f0c6bc8f71e1
Sebastian Podjasek <sebastian.podjasek@intelliway.pl>
2024-10-16 11:08:40 +0200

    Refactor export statement in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 // We use cryptographically strong PRNGs (crypto.getRandomBytes())
 // When using crypto.getRandomValues(), our primitive is hexString(),
 // from which we construct fraction().
-import RandomGenerator from './AbstractRandomGenerator.js';
 import createRandom from './createRandom.js';
 import NodeRandomGenerator from './NodeRandomGenerator.js';
 
 const Random = createRandom(new NodeRandomGenerator());
-
-export { Random as default, type RandomGenerator };
+export default Random;
